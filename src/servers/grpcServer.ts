7bd6bf9c99055721ad1e9d3d717a84e2f8df8273
Rawsab Said <rawsab04@gmail.com>
2025-11-20 11:11:19 -0500

    add log statements to endpoints

--- src/servers/grpcServer.ts
+++ src/servers/grpcServer.ts
@@ -18,62 +18,88 @@ import {
     DeepPartial,
 } from '../compiled_proto/app';
 import { IDatabase, IServer } from '../interfaces';
+import logger from '../logger';
 
 import { createServer } from 'nice-grpc';
 
 class GrpcServiceImpl implements AppServiceImplementation {
     db: IDatabase;
+    requestCount: number = 0;
 
     constructor(db: IDatabase) {
         this.db = db;
     }
 
     async getProduct(request: ProductRequest): Promise<DeepPartial<Product>> {
+        this.requestCount++;
+        logger.info(`[Request #${this.requestCount}] Incoming gRPC getProduct - request: ${JSON.stringify(request)}`);
         const product = await this.db.queryProductById(request.productId);
         return product;
     }
     async getRandomProduct(request: EmptyRequest): Promise<DeepPartial<Product>> {
+        this.requestCount++;
+        logger.info(`[Request #${this.requestCount}] Incoming gRPC getRandomProduct - request: ${JSON.stringify(request)}`);
         const product = await this.db.queryRandomProduct();
         return product;
     }
     async getAllProducts(request: AllProductsRequest): Promise<DeepPartial<Products>> {
+        this.requestCount++;
+        logger.info(`[Request #${this.requestCount}] Incoming gRPC getAllProducts - request: ${JSON.stringify(request)}`);
         const products = await this.db.queryAllProducts(request.categoryId);
         return { products };
     }
     async getAllCategories(request: EmptyRequest): Promise<DeepPartial<Categories>> {
+        this.requestCount++;
+        logger.info(`[Request #${this.requestCount}] Incoming gRPC getAllCategories - request: ${JSON.stringify(request)}`);
         const categories = await this.db.queryAllCategories();
         return { categories };
     }
     async getAllOrders(request: EmptyRequest): Promise<DeepPartial<Orders>> {
+        this.requestCount++;
+        logger.info(`[Request #${this.requestCount}] Incoming gRPC getAllOrders - request: ${JSON.stringify(request)}`);
         const orders = await this.db.queryAllOrders();
         return { orders };
     }
     async getAllUserOrders(request: UserRequest): Promise<DeepPartial<Orders>> {
+        this.requestCount++;
+        logger.info(`[Request #${this.requestCount}] Incoming gRPC getAllUserOrders - request: ${JSON.stringify(request)}`);
         const orders = await this.db.queryOrdersByUser(request.id);
         return { orders };
     }
     async getOrder(request: OrderRequest): Promise<DeepPartial<Order>> {
+        this.requestCount++;
+        logger.info(`[Request #${this.requestCount}] Incoming gRPC getOrder - request: ${JSON.stringify(request)}`);
         const order = await this.db.queryOrderById(request.id);
         return order;
     }
     async getUser(request: UserRequest): Promise<DeepPartial<User>> {
+        this.requestCount++;
+        logger.info(`[Request #${this.requestCount}] Incoming gRPC getUser - request: ${JSON.stringify(request)}`);
         const user = await this.db.queryUserById(request.id);
         return user;
     }
     async getAllUsers(request: EmptyRequest): Promise<DeepPartial<Users>> {
+        this.requestCount++;
+        logger.info(`[Request #${this.requestCount}] Incoming gRPC getAllUsers - request: ${JSON.stringify(request)}`);
         const users = await this.db.queryAllUsers();
         return { users };
     }
     async postOrder(request: Order): Promise<DeepPartial<Order>> {
+        this.requestCount++;
+        logger.info(`[Request #${this.requestCount}] Incoming gRPC postOrder - request: ${JSON.stringify(request)}`);
         await this.db.insertOrder(request);
         return request;
     }
     async patchAccountDetails(request: UserPatchRequest): Promise<DeepPartial<User>> {
+        this.requestCount++;
+        logger.info(`[Request #${this.requestCount}] Incoming gRPC patchAccountDetails - request: ${JSON.stringify(request)}`);
         await this.db.updateUser(request);
         const user = await this.db.queryUserById(request.id);
         return user;
     }
     async deleteOrder(request: OrderRequest): Promise<DeepPartial<EmptyResponse>> {
+        this.requestCount++;
+        logger.info(`[Request #${this.requestCount}] Incoming gRPC deleteOrder - request: ${JSON.stringify(request)}`);
         const { id } = request;
         try {
           await this.db.deleteOrder(id);
@@ -97,6 +123,6 @@ export default class GrpcServer implements IServer {
         const port = 3001;
         this.server.add(AppDefinition, new GrpcServiceImpl(this.db));
         await this.server.listen(`0.0.0.0:${port}`);
-        console.log(`gRPC server listening on port ${port}`);
+        logger.info(`gRPC server listening on port ${port}`);
     }
 };
--- src/servers/restServer.ts
+++ src/servers/restServer.ts
@@ -10,6 +10,7 @@ export default class RestServer implements IServer {
 
   db: IDatabase;
   server: any;
+  requestCount: number = 0;
 
   constructor(db: IDatabase) {
     this.db = db;
@@ -22,9 +23,15 @@ export default class RestServer implements IServer {
     this.server.use(morgan("tiny"));
     this.server.use(bodyParser.json());
 
-    this.server.get("/", (req: express.Request, res: express.Response) => res.send("Hello, World!"));
+    this.server.get("/", (req: express.Request, res: express.Response) => {
+      this.requestCount++;
+      logger.info(`[Request #${this.requestCount}] Incoming GET / - params: ${JSON.stringify(req.params)}, query: ${JSON.stringify(req.query)}`);
+      res.send("Hello, World!");
+    });
 
     this.server.get("/product/:productId", async (req: express.Request, res: express.Response) => {
+      this.requestCount++;
+      logger.info(`[Request #${this.requestCount}] Incoming GET /product/:productId - params: ${JSON.stringify(req.params)}, query: ${JSON.stringify(req.query)}`);
       const { productId } = (req.params as ProductRequest);
       if (!productId) {
         res.status(400).send("No product id provided");
@@ -35,27 +42,37 @@ export default class RestServer implements IServer {
     }); // Gets a product by product id
 
     this.server.get("/randomproduct", async (req: express.Request, res: express.Response) => {
+      this.requestCount++;
+      logger.info(`[Request #${this.requestCount}] Incoming GET /randomproduct - params: ${JSON.stringify(req.params)}, query: ${JSON.stringify(req.query)}`);
       const randProd = await this.db.queryRandomProduct();
       res.send(randProd);
     }); // I'm feeling lucky type
 
     this.server.get("/products", async (req: express.Request, res: express.Response) => {
+      this.requestCount++;
+      logger.info(`[Request #${this.requestCount}] Incoming GET /products - params: ${JSON.stringify(req.params)}, query: ${JSON.stringify(req.query)}`);
       const { categoryId } = (req.query as AllProductsRequest);
       const products = await this.db.queryAllProducts(categoryId);
       res.send(products);
     }); // Gets all products, or by category
 
     this.server.get("/categories", async (req: express.Request, res: express.Response) => {
+      this.requestCount++;
+      logger.info(`[Request #${this.requestCount}] Incoming GET /categories - params: ${JSON.stringify(req.params)}, query: ${JSON.stringify(req.query)}`);
       const categories = await this.db.queryAllCategories();
       res.send(categories);
     }); // Gets all categories
 
     this.server.get("/allorders", async (req: express.Request, res: express.Response) => {
+      this.requestCount++;
+      logger.info(`[Request #${this.requestCount}] Incoming GET /allorders - params: ${JSON.stringify(req.params)}, query: ${JSON.stringify(req.query)}`);
       const orders = await this.db.queryAllOrders();
       res.send(orders);
     }); // Gets all orders
 
     this.server.get("/orders", async (req: express.Request, res: express.Response) => {
+      this.requestCount++;
+      logger.info(`[Request #${this.requestCount}] Incoming GET /orders - params: ${JSON.stringify(req.params)}, query: ${JSON.stringify(req.query)}`);
       const { id } = (req.query as UserRequest);
       if (!id) {
         res.status(400).send("No user id provided");
@@ -66,6 +83,8 @@ export default class RestServer implements IServer {
     }); // Gets all of a single user's orders
 
     this.server.get("/order/:id", async (req: express.Request, res: express.Response) => {
+      this.requestCount++;
+      logger.info(`[Request #${this.requestCount}] Incoming GET /order/:id - params: ${JSON.stringify(req.params)}, query: ${JSON.stringify(req.query)}`);
       const { id } = (req.params as OrderRequest);
       if (!id) {
         res.status(400).send("No order id provided");
@@ -76,6 +95,8 @@ export default class RestServer implements IServer {
     }); // Gets more details on a specific order by id
 
     this.server.get("/user/:id", async (req: express.Request, res: express.Response) => {
+      this.requestCount++;
+      logger.info(`[Request #${this.requestCount}] Incoming GET /user/:id - params: ${JSON.stringify(req.params)}, query: ${JSON.stringify(req.query)}`);
       const { id } = (req.params as UserRequest);
       if (!id) {
         res.status(400).send("No user id provided");
@@ -85,18 +106,24 @@ export default class RestServer implements IServer {
       res.send(user);
     }); // Gets details on a specific user by username
 
-    this.server.get("/users", async (_req: express.Request, res: express.Response) => {
+    this.server.get("/users", async (req: express.Request, res: express.Response) => {
+      this.requestCount++;
+      logger.info(`[Request #${this.requestCount}] Incoming GET /users - params: ${JSON.stringify(req.params)}, query: ${JSON.stringify(req.query)}`);
       const users = await this.db.queryAllUsers();
       res.send(users);
     });// Gets all users
 
     this.server.post("/orders", async (req: express.Request, res: express.Response) => {
+      this.requestCount++;
+      logger.info(`[Request #${this.requestCount}] Incoming POST /orders - params: ${JSON.stringify(req.params)}, query: ${JSON.stringify(req.query)}, body: ${JSON.stringify(req.body)}`);
       const order = (req.body as Order);
       const response = await this.db.insertOrder(order);
       res.send(response);
     }); // Creates a new order
 
     this.server.patch("/user/:id", async (req: express.Request, res: express.Response) => {
+      this.requestCount++;
+      logger.info(`[Request #${this.requestCount}] Incoming PATCH /user/:id - params: ${JSON.stringify(req.params)}, query: ${JSON.stringify(req.query)}, body: ${JSON.stringify(req.body)}`);
       const updates = req.body;
       const userId = (req.params as UserRequest).id;
       const patch: UserPatchRequest = {
@@ -108,6 +135,8 @@ export default class RestServer implements IServer {
     }); // Updates a user's email or password
 
     this.server.delete("/order/:id", async (req: express.Request, res: express.Response) => {
+      this.requestCount++;
+      logger.info(`[Request #${this.requestCount}] Incoming DELETE /order/:id - params: ${JSON.stringify(req.params)}, query: ${JSON.stringify(req.query)}`);
       const { id } = req.params as OrderRequest;
       if (!id) {
         res.status(400).send("No order id provided");
